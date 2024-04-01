61ddd652d8610ba0e0fabe8b72f2161f63c32485
riznalx390yoga <pcxyoga@gmail.com>
2024-04-01 23:50:26 +0700

    tugas riznal p11

---
+++ dashboard.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Riztech</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+  <div class="wrapper">
+
+
+    <?php include "navbar.php"; ?>
+
+    <?php include "sidebar.php"; ?>
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Program Yang Sedang Diikuti</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding1.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Pemrograman Web Dasar</h5>
+                  <p class="card-text">Pembelajaran tentang HTML, CSS, JavaScript untuk membangun situs web sederhana dan interaktif.</p>
+                  <a href="#" class="btn btn-primary w-100">Cek Progres</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+      <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+      </div>
+    </aside>
+
+
+    <?php include "footer.php"; ?>
+
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- REQUIRED SCRIPTS -->
+
+  <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
---
+++ newproduct.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Riztech</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+  < class="wrapper">
+
+    <?php include "navbar.php"; ?>
+
+    <?php include "sidebar.php"; ?>
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Daftar Coding Camp</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <div class="content">
+        <div class="container">
+          <div class="row g-5">
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding1.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Pemrograman Web Dasar</h5>
+                  <p class="card-text">Pembelajaran tentang HTML, CSS, JavaScript untuk membangun situs web sederhana dan interaktif.</p>
+                  <a href="#" class="btn btn-primary w-100">DAFTAR SEKARANG</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding2.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Pemrograman Python</h5>
+                  <p class="card-text">Pengantar ke bahasa pemrograman Python dari dasar hingga pengembangan aplikasi skala besar.</p>
+                  <a href="#" class="btn btn-primary w-100">DAFTAR SEKARANG</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding3.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Pengembangan Aplikasi Mobile </h5>
+                  <p class="card-text">Membuat aplikasi mobile dengan Flutter atau React Native, fokus pada UI/UX dan fungsionalitas.</p>
+                  <a href="#" class="btn btn-primary w-100">DAFTAR SEKARANG</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding4.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Pengembangan Aplikasi Android</h5>
+                  <p class="card-text">Memahami pembuatan aplikasi Android menggunakan Java atau Kotlin, termasuk integrasi API dan fitur Android.</p>
+                  <a href="#" class="btn btn-primary w-100">DAFTAR SEKARANG</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding5.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Ilmu Data Dasar</h5>
+                  <p class="card-text">Analisis data menggunakan Python, Pandas, visualisasi data, dan pengantar pembelajaran mesin.</p>
+                  <a href="#" class="btn btn-primary w-100">DAFTAR SEKARANG</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding6.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Pengembangan Permainan</h5>
+                  <p class="card-text">Pembuatan permainan dengan Unity atau Unreal Engine, fokus pada desain, pengembangan gameplay, dan grafis.</p>
+                  <a href="#" class="btn btn-primary w-100">DAFTAR SEKARANG</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding7.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Keamanan Siber</h5>
+                  <p class="card-text">Memahami konsep keamanan jaringan, etika hacking, penetrasi tes, perlindungan data, serta strategi mitigasi risiko cyber.</p>
+                  <a href="#" class="btn btn-primary w-100">DAFTAR SEKARANG</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding8.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Pengembangan Situs E-commerce</h5>
+                  <p class="card-text">Pembuatan situs e-commerce dengan Laravel atau CodeIgniter, termasuk manajemen produk, checkout, dan pembayaran.</p>
+                  <a href="#" class="btn btn-primary w-100">DAFTAR SEKARANG</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="card">
+                <img src="gambarnewproduct/coding9.png" class="card-img-top align-self-center" style="width: 200px;">
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">Pengembangan Aplikasi Berbasis Cloud</h5>
+                  <p class="card-text">Membuat aplikasi berbasis cloud dengan integrasi layanan AWS, Google Cloud, atau Azure.</p>
+                  <a href="#" class="btn btn-primary w-100">DAFTAR SEKARANG</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+      <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+      </div>
+    </aside>
+    <!-- /.control-sidebar -->
+
+    <?php include "footer.php"; ?>
+
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
